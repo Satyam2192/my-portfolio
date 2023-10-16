ed2863b994993db53f0af12d673be2e38dd4f9ed
Satyam2192 <Satyam21092@gmail.com>
2023-10-16 14:04:44 +0530

    added footer and contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 py-10 max-w-7xl text-white ">
-      <div className="container mx-auto flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between">
+    <footer className="bg-gray-900 py-10  text-white ">
+      <div className="container mx-auto flex max-w-8xl flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between">
         <NavLink
           className="mb-8 text-2xl font-semibold p-2 border-4 border-pink-500 hover:border-purple-500 md:mb-4 sm:mb-0"
           to="/contact"
